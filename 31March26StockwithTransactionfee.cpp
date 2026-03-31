45ecaa85cf7a041c1e8713d4b37f4301f8fb0459
rajeshthakur30 <rajeshthakur0905@gmail.com>
2026-03-31 23:58:47 +0530

    31Marcch26

---
+++ 31March26StockwithTransactionfee.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int maxProfit(vector<int>& arr, int k) {
+        // Code here
+        int n = arr.size();
+        
+        int hold = -arr[0];
+        
+        int cash = 0;
+        
+        for (int i = 1; i < n; i++) {
+            int prevCash = cash;
+            
+            hold = max(hold, prevCash - arr[i]);
+            
+            cash = max(cash, hold + arr[i] - k);
+        }
+        
+        return cash;
+    }
+  };
